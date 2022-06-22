d56960d1bb51e93bffd7bb32a86fe24ae5740470
wahyuPM <wahyupratama191@gmail.com>
2022-06-22 17:05:30 +0800

    add : carousel logic

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -11,9 +11,105 @@ const carouselCalculateOffset = () => {
 }
 
 const slide = (wrapper, items) => {
-    let posX1 = 0, posX2 = 0, posInitial, posFinal, threshold = 100, slides = items.children;
+    let posX1 = 0, posX2 = 0, posInitial, posFinal, threshold = 100, itemToShow = 4, slides = items.getElementsByClassName("card"), slidesLength = slides.length, slideSize = items.getElementsByClassName("card")[0].offsetWidth, index = 0, allowShift = true;
+
+    const dragStart = (e) => {
+        e = e || window.event
+        e.preventDefault()
+        posInitial = items.offsetLeft
+
+        if (e.type == "touchstart") {
+            posX1 = e.touches[0].clientX
+        } else {
+            posX1 = e.clientX
+            document.onmouseup = dragEnd
+            document.onmousemove = dragAction
+        }
+    }
+
+    const dragAction = (e) => {
+        e = e || window.event
+        e.preventDefault()
+
+        if (e.type == "touchmove") {
+            posX2 = posX1 - e.touches[0].clientX
+            posX1 = e.touches[0].clientX
+        } else {
+            posX2 = posX1 - e.clientX
+            posX1 = e.clientX
+        }
+
+        items.style.left = `${items.offsetLeft - posX2}px`
+    }
+
+    const dragEnd = () => {
+        posFinal = items.offsetLeft
+
+        if (posFinal - posInitial < -threshold) {
+            shiftSlide(1, "drag")
+        } else if (posFinal - posInitial > threshold) {
+            shiftSlide(-1, "drag")
+        } else {
+            items.style.left = `${posInitial}px`
+        }
+
+        document.onmouseup = null
+        document.onmousemove = null
+    }
+
+    const shiftSlide = (direction, action) => {
+        addClass(items, "transition-all duration-200")
+        if (allowShift) {
+            if (!action) posInitial = items.offsetLeft
+
+            if (direction == 1) {
+                items.style.left = `${posInitial - slideSize}px`
+                index++
+            } else if (direction == -1) {
+                items.style.left = `${posInitial + slideSize}px`
+                index--
+            }
+        }
+        allowShift = false
+    }
+
+    const checkIndex = () => {
+        setTimeout(() => {
+            removeClass(items, "transition-all duration-200")
+        }, 200)
+
+        if (index == -1) {
+            items.style.left = -(slidesLength * slideSize) + "px"
+            index = slidesLength - 1
+        }
+
+        if (index == slidesLength - itemToShow) {
+            items.style.left = -((slidesLength - itemToShow - 1) * slideSize) + "px"
+            index = slidesLength - itemToShow - 1
+        }
+
+        if (index == slidesLength || index == slidesLength - 1) {
+            items.style.left = "0px"
+            index = 0
+        }
+
+        allowShift = true
+    }
+
+    wrapper.classList.add("loaded")
+
+    items.onmousedown = dragStart;
+
+    items.addEventListener("touchstart", dragStart)
+    items.addEventListener("touchend", dragEnd)
+    items.addEventListener("touchmove", dragAction)
+
+    items.addEventListener("transitionend", checkIndex)
+
 }
+
 if (carouselId) {
+    slide(carouselId, carouselItems);
     window.addEventListener("load", carouselCalculateOffset);
     window.addEventListener("resize", carouselCalculateOffset);
 }
